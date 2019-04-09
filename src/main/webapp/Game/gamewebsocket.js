8cf2aec3bd602646b6d890fed80b980a351bbf83
dal79 <dal79@users.noreply.github.com>
2019-04-09 10:41:00 -0500

    Testing new files
    Will revert after testing

---
+++ src/main/webapp/Game/gamewebsocket.js
@@ -0,0 +1,44 @@
+var url = (window.location.protocol === "https:" ? "wss:" : "ws:") + "//" + window.location.host + window.location.pathname + "game-endpoint";
+
+//set up websocket
+var webSocket = new WebSocket(url);
+webSocket.onopen = function () {
+    console.log("WebSocket is connected.");
+    webSocket.send("Connect");
+    console.log(event.data);
+};
+webSocket.onmessage = function (event) {
+    console.log(event.data);
+    var str = event.data;
+    if (str.startsWith("JOINED")) {
+        var arr = str.split(":");
+        document.getElementById("hlog").innerHTML(arr[1] + " has joined.");
+        document.getElementById("jlog").innerHTML("You have joined lobby " + arr[0] + "..");
+    }
+    
+};
+
+//get messages function
+function getMessages() {
+    webSocket.send("GET");
+}
+
+//stop messages function
+function stopMessages() {
+    webSocket.send("STOP");
+    document.getElementById("btnGet").disabled = false;
+    document.getElementById("btnStop").disabled = true;
+}
+
+function getSessionId() {
+    document.getElementById("givenID").innerText = document.getElementById("gameID").value;
+    webSocket.send("GAMEID:" + document.getElementById("gameID").value);
+}
+
+function getClientId() {
+    webSocket.send("JOIN:" + document.getElementById("joinerID").value + ":" + document.getElementById("joinerName").value);
+}
+
+function userList(){
+  	webSocket.send("USER");
+}

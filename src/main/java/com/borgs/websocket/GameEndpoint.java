e514d805800ceb965ecdf0c0a2cdae11f7152bbe
dal79 <dal79@users.noreply.github.com>
2019-04-27 22:20:47 -0500

    Update GameEndpoint.java
    comment out OnError

--- src/main/java/com/borgs/websocket/GameEndpoint.java
+++ src/main/java/com/borgs/websocket/GameEndpoint.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 
 import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
+//import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -152,8 +152,5 @@ public class GameEndpoint {
         
     }
 
-    @OnError
-    public void onError(Session session) {
-
-    }
+    //@OnError public void onError(Session session) { }
 }
